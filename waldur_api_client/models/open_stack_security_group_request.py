2c030d961fc7fa90d977809d163a6e645fcf8304
Waldur Gitlab Runner <ci@waldur.com>
2025-06-11 16:46:01 +0000

    Update Waldur Python SDK

--- waldur_api_client/models/open_stack_security_group_request.py
+++ waldur_api_client/models/open_stack_security_group_request.py
@@ -1,9 +1,13 @@
-from typing import Any, TypeVar, Union
+from typing import TYPE_CHECKING, Any, TypeVar, Union
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
 
 from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.open_stack_security_group_rule_create_request import OpenStackSecurityGroupRuleCreateRequest
+
 
 T = TypeVar("T", bound="OpenStackSecurityGroupRequest")
 
@@ -13,15 +17,22 @@ class OpenStackSecurityGroupRequest:
     """
     Attributes:
         name (str):
+        rules (list['OpenStackSecurityGroupRuleCreateRequest']):
         description (Union[Unset, str]):
     """
 
     name: str
+    rules: list["OpenStackSecurityGroupRuleCreateRequest"]
     description: Union[Unset, str] = UNSET
     additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> dict[str, Any]:
         name = self.name
+
+        rules = []
+        for rules_item_data in self.rules:
+            rules_item = rules_item_data.to_dict()
+            rules.append(rules_item)
 
         description = self.description
 
@@ -30,6 +41,7 @@ class OpenStackSecurityGroupRequest:
         field_dict.update(
             {
                 "name": name,
+                "rules": rules,
             }
         )
         if description is not UNSET:
@@ -39,13 +51,23 @@ class OpenStackSecurityGroupRequest:
 
     @classmethod
     def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
+        from ..models.open_stack_security_group_rule_create_request import OpenStackSecurityGroupRuleCreateRequest
+
         d = src_dict.copy()
         name = d.pop("name")
+
+        rules = []
+        _rules = d.pop("rules")
+        for rules_item_data in _rules:
+            rules_item = OpenStackSecurityGroupRuleCreateRequest.from_dict(rules_item_data)
+
+            rules.append(rules_item)
 
         description = d.pop("description", UNSET)
 
         open_stack_security_group_request = cls(
             name=name,
+            rules=rules,
             description=description,
         )
 
