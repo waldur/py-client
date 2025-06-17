2a7d75b5b61fb83c0dfa0d227679ddb0cdfdf3e0
Waldur Gitlab Runner <ci@waldur.com>
2025-06-17 21:08:09 +0000

    Update Waldur Python SDK

--- waldur_api_client/models/user_invitations_list_o_item.py
+++ waldur_api_client/models/user_invitations_list_o_item.py
@@ -3,11 +3,13 @@ from enum import Enum
 
 class UserInvitationsListOItem(str, Enum):
     CREATED = "created"
+    CREATED_BY = "created_by"
     EMAIL = "email"
     STATE = "state"
     VALUE_0 = "-created"
-    VALUE_1 = "-email"
-    VALUE_2 = "-state"
+    VALUE_1 = "-created_by"
+    VALUE_2 = "-email"
+    VALUE_3 = "-state"
 
     def __str__(self) -> str:
         return str(self.value)
