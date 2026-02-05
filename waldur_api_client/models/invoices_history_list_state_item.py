32e3b5529ef0641b08504cf63c4f0c32bef1b863
Ilja Livenson <ilja@opennodecloud.com>
2026-02-05 17:28:37 +0000

    Update Waldur Python SDK

--- waldur_api_client/models/invoices_history_list_state_item.py
+++ waldur_api_client/models/invoices_history_list_state_item.py
@@ -6,6 +6,7 @@ class InvoicesHistoryListStateItem(str, Enum):
     CREATED = "created"
     PAID = "paid"
     PENDING = "pending"
+    PENDING_FINALIZATION = "pending_finalization"
 
     def __str__(self) -> str:
         return str(self.value)
