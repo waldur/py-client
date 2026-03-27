4fb2be166608b6b75ae78c9b3afbb1b7ce50b6a9
Viktor Mirieiev <victor.mireyev@gmail.com>
2026-03-27 21:20:10 +0000

    Update Waldur Python SDK

--- waldur_api_client/models/software_target_o_enum.py
+++ waldur_api_client/models/software_target_o_enum.py
@@ -2,18 +2,16 @@ from enum import Enum
 
 
 class SoftwareTargetOEnum(str, Enum):
-    CPU_FAMILY = "cpu_family"
     CPU_MICROARCHITECTURE = "cpu_microarchitecture"
     CREATED = "created"
     PACKAGE_NAME = "package_name"
     TARGET_NAME = "target_name"
     TARGET_TYPE = "target_type"
-    VALUE_0 = "-cpu_family"
-    VALUE_1 = "-cpu_microarchitecture"
-    VALUE_2 = "-created"
-    VALUE_3 = "-package_name"
-    VALUE_4 = "-target_name"
-    VALUE_5 = "-target_type"
+    VALUE_0 = "-cpu_microarchitecture"
+    VALUE_1 = "-created"
+    VALUE_2 = "-package_name"
+    VALUE_3 = "-target_name"
+    VALUE_4 = "-target_type"
 
     def __str__(self) -> str:
         return str(self.value)
