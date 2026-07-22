37dda1f9d7fd21632b06fba79596816ac6fe0842
Viktor Mirieiev <victor.mireyev@gmail.com>
2026-07-22 20:57:39 +0000

    chore: nightly update of Waldur Python SDK

--- waldur_api_client/models/offering_user_o_enum.py
+++ waldur_api_client/models/offering_user_o_enum.py
@@ -5,9 +5,13 @@ class OfferingUserOEnum(str, Enum):
     CREATED = "created"
     MODIFIED = "modified"
     USERNAME = "username"
+    USER_FIRST_NAME = "user_first_name"
+    USER_LAST_NAME = "user_last_name"
     VALUE_0 = "-created"
     VALUE_1 = "-modified"
-    VALUE_2 = "-username"
+    VALUE_2 = "-user_first_name"
+    VALUE_3 = "-user_last_name"
+    VALUE_4 = "-username"
 
     def __str__(self) -> str:
         return str(self.value)
